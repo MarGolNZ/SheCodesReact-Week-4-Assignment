6c5412ad63ccb54522d686743418e7d18d6c0228
MarGolNZ <54025171+MarGolNZ@users.noreply.github.com>
2021-04-18 20:22:38 +1000

    Add footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Search />
+      <footer>
+        <p>
+
+         <a href="https://github.com/MarGolNZ/SheCodesReact-Week-4-Assignment" target="_blank">
+Open-source  
+        </a> code by  <a href="https://github.com/MarGolNZ" target="_blank">marina-golovina</a>
+        </p>
+        
+
+        </footer>
+        
     </div>
   );
 }
